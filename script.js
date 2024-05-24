ad9dfb89b875a80a6918f9f865f644e57e64cab0
Michi <84442219+mab-mab@users.noreply.github.com>
2024-05-24 19:41:20 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
-alert("aaa")
 
-// This method will trigger user permissions
+/*  This method will trigger user permissions
 Html5Qrcode.getCameras().then(devices => {
     console.log(devices);
-    /**
-     * devices would be an array of objects of type:
-     * { id: "id", label: "label" }
-     */
     if (devices && devices.length) {
       let cameraId = devices[0].id;
       console.log(cameraId);
@@ -33,7 +28,14 @@ Html5Qrcode.getCameras().then(devices => {
     }
   }).catch(err => {
     console.log(err);
-  });
+  });*/
+
+const html5QrCode = new Html5Qrcode("reader");
+const qrCodeSuccessCallback = (decodedText, decodedResult) => {
+    /* handle success */
+};
+const config = { fps: 10, qrbox: { width: 250, height: 250 } };
+html5QrCode.start({ facingMode: "environment" }, config, qrCodeSuccessCallback);
 
 
 
